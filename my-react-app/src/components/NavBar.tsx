909892218648da4797b4ecb402360caebd6093a3
MikeHatchi22 <hatchi.mike@gmail.com>
2024-10-24 16:50:55 +0200

    Create button list

--- my-react-app/src/components/NavBar.tsx
+++ my-react-app/src/components/NavBar.tsx
@@ -9,21 +9,14 @@ interface NavBarProps {
     pokemonList: Pokemon[];
 }
 
-function NavBar({ pokemonIndex, setPokemonIndex, pokemonList }: NavBarProps) {
-    const handleClickNext = () => {
-        setPokemonIndex(pokemonIndex + 1);
-    }
-
-    const handleClickPrev = () => {
-        setPokemonIndex(pokemonIndex - 1)
-    }
+function NavBar({ setPokemonIndex, pokemonList }: NavBarProps) {
     return (
         <>
-            {pokemonIndex === 0 ? <button onClick={handleClickPrev} hidden>Précédent</button>
-                : <button onClick={handleClickPrev}>Précédent</button>}
-
-            {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClickNext}>Suivant</button>
-                : <button onClick={handleClickNext} hidden>Suivant</button>}
+            {pokemonList.map((pokemon, index) => (
+                <button onClick={() => setPokemonIndex(index)} key={index}>
+                    {pokemon.name}
+                </button>
+            ))}
         </>
     );
 }
